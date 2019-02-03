aed5e541cfa8cc103d6239f471c1b55fd2192b88
lahul <lahuldcosta04@gmail.com>
2019-02-03 20:50:16 +0530

    Before eject

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Components/Home/Home'
+import Layout from './hoc/layout/layout'
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <Layout>
+        <Switch>
+            <Route path='/' exact component={Home}/>      
+        </Switch>
+      </Layout> 
+    )
+  }
+}
